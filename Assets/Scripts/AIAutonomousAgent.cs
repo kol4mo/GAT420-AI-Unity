0a32b67cb14250ce7a539b21c24ba1bf925ae36e
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-01-09 15:24:20 -0700

    added AI

---
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAutonomousAgent : AIAgent {
+    public AIPerception perception = null;
+
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/AIDistancePerception.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDistancePerception : AIPerception {
+
+    public override GameObject[] GetGameObjects() {
+        throw new System.NotImplementedException();
+    }
+}
